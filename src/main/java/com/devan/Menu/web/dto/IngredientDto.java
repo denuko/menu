4f5168bccb58adfd316479cd420e0a5844297747
Despoina Nyuu <nyuu_despoina@hotmail.com>
2018-12-16 20:01:30 +0200

    Add swagger2

--- src/main/java/com/devan/Menu/web/dto/IngredientDto.java
+++ src/main/java/com/devan/Menu/web/dto/IngredientDto.java
@@ -1,6 +1,8 @@
 package com.devan.Menu.web.dto;
 
 import com.devan.Menu.dao.enums.IngredientType;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +11,7 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.Instant;
 
+@ApiModel(description = "Ingredient details")
 @Getter
 @Setter
 public class IngredientDto implements Serializable {
@@ -16,9 +19,11 @@ public class IngredientDto implements Serializable {
     private Long id;
 
     @NotBlank
+    @ApiModelProperty(notes = "Required", example = "Bacon", required = true)
     private String name;
 
     @NotNull
+    @ApiModelProperty(notes = "Required", example = "MEAT", required = true)
     private IngredientType type;
 
     private Instant postedAt;
